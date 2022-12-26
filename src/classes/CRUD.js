7880e2e433283c6bfd69b76ed8d6282cbc601465
magna <julijagarkusha@gmail.com>
2022-12-26 19:05:52 +0200

    added domashka 20

--- src/classes/CRUD.js
+++ src/classes/CRUD.js
@@ -3,67 +3,60 @@ class CRUD {
         this.apiUrl = apiUrl;
     }
 
-    validateResponse(response) {
-        if (response.status >= 400) {
-            throw new Error('Network error');
-        }
-    }
-
     async get(path) {
-       try {
-           const response = await fetch(path ? `${this.apiUrl}/${path}`: this.apiUrl);
-           this.validateResponse(response);
-           return response.json();
-       } catch(exception) {
-           console.log('debug exception: ', exception);
-           return [];
-       }
+        const response = await fetch(path ? `${this.apiUrl}/${path}`: this.apiUrl);
+
+        if (response.ok) {
+            return response.json();
+        }
+
+        console.error('debug exception: ', 'Network error');
     }
 
     async update(path, data) {
-        try {
-            const response = await fetch(path ? `${this.apiUrl}/${path}`: this.apiUrl, {
-                method: 'PUT',
-                body: JSON.stringify(data),
-                headers: {
-                    'Content-type': 'application/json',
-                }
-            });
-            this.validateResponse(response);
+        const response = await fetch(path ? `${this.apiUrl}/${path}`: this.apiUrl, {
+            method: 'PUT',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-type': 'application/json',
+            }
+        });
+
+        if (response.ok) {
             return response.json();
-        } catch(exception) {
-            console.log('debug exception: ', exception);
         }
+
+        console.error('debug exception: ', 'Network error');
     }
 
     async create(path, data) {
-        try {
-            const response = await fetch(path ? `${this.apiUrl}/${path}`: this.apiUrl, {
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers: {
-                    'Content-type': 'application/json',
-                }
-            });
-            this.validateResponse(response);
+        const response = await fetch(path ? `${this.apiUrl}/${path}`: this.apiUrl, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-type': 'application/json',
+            }
+        });
+
+        if (response.ok) {
             return response.json();
-        } catch(exception) {
-            console.log('debug exception: ', exception);
         }
+
+        console.error('debug exception: ', 'Network error');
     }
 
     async remove(path) {
-        try {
-            const response = await fetch(path ? `${this.apiUrl}/${path}`: this.apiUrl, {
-                method: 'DELETE',
-                headers: {
-                    'Content-type': 'application/json',
-                }
-            });
-            this.validateResponse(response);
+        const response = await fetch(path ? `${this.apiUrl}/${path}`: this.apiUrl, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json',
+            }
+        });
+
+        if (response.ok) {
             return response.json();
-        } catch(exception) {
-            console.log('debug exception: ', exception);
         }
+
+        console.error('debug exception: ', 'Network error');
     }
 }
